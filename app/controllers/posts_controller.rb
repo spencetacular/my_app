381589ed794da89028661a7ac42e18083e828a22
Spencer Christopherson <schristopherson@gmail.com>
2015-12-02 14:39:53 -0700

    edits posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
   
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to posts_path
+		else
+			render :edit
+		end
+	end
+
   def create
   	@post = Post.new(post_params)
   	if @post.save
