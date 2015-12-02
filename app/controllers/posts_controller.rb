06ead64e696c1b6df36c3e3a8fd14760bc05af7b
Spencer Christopherson <schristopherson@gmail.com>
2015-12-02 15:13:05 -0700

    fixed conflict

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,12 @@ class PostsController < ApplicationController
   	end
   end
 
+  def destroy
+  	@post = Post.find(params[:id])
+  	@post.destroy
+  	redirect_to posts_path
+  end
+
   def index
   	@posts = Post.all
   end
